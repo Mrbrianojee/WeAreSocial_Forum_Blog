79e8a5cefd3a893af6694d5a11cd3bfb826639b4
Mrbrianojee <brian@codeinstitute.net>
2016-01-12 17:47:33 +0000

    Added threads related files

---
+++ threads/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from tinymce.models import HTMLField
+from django.conf import settings
+from django.utils import timezone
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=254)
+    description = HTMLField()
+
+    def __unicode__(self):
+        return self.name
+
+
+class Thread(models.Model):
+    name = models.CharField(max_length=254)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='threads')
+    subject = models.ForeignKey(Subject, related_name='threads')
+    created_at = models.DateField(default=timezone.now)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Posts(models.Model):
+    thread = models.ForeignKey(Thread, related_name='posts')
+    comment = HTMLField(blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='posts')
+    created_at = models.DateField(default=timezone.now)
+
+    def __unicode__(self):
+        return self.comment
